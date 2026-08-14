4200c7d2cdc4a95e7ef0415aa67d72b736f8e3f1
Anton Melnikov <melnikov.anton@gmail.com>
2026-08-14 10:09:26 +0300

    Deps update

--- src/tools/mcpClients/MCPClient.ts
+++ src/tools/mcpClients/MCPClient.ts
@@ -1,5 +1,5 @@
-import { Client } from "@modelcontextprotocol/sdk/client/index.js";
-import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import { Client } from "@modelcontextprotocol/client";
+import { StdioClientTransport } from "@modelcontextprotocol/client/stdio";
 import type OpenAI from "openai";
 import z from "zod";
 
